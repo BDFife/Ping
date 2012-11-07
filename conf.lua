52bd0257960138571797088e6d8c31179341cf73
Brian Fife <bdfife@gmail.com>
2012-11-06 22:18:58 -0500

    Updated lua conf to 8.0 settings

--- conf.lua
+++ conf.lua
@@ -2,17 +2,18 @@
 function love.conf(t)
     t.title = "Table Tennis Trainer"
     t.author = "Brian Fife"
+    t.url = nil
     t.identity = nil
-    t.version = "0.7.2"
+    t.version = "0.8.0"
     t.console = false
     t.release = false
     t.screen.width = 800
     t.screen.height = 600
-    t.sync.fullscreen = false
+    t.screen.fullscreen = false
     t.screen.vsync = true
     t.screen.fsaa = 0
     t.modules.joystick = false
-    t.modules.audio = false
+    t.modules.audio = true
     t.modules.keyboard = true
     t.modules.event = true
     t.modules.image = false
