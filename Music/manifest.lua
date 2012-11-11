dde1cf5bdfe240087908588c36e2003a47629c2e
Brian Fife <bdfife@gmail.com>
2012-11-11 10:23:02 -0500

    Adding default manifest.

---
+++ Music/manifest.lua
@@ -0,0 +1,13 @@
+
+function load_playlist()
+	return {{ "Trouba",
+		      "Trouba.lua"
+		    },
+		    { "Trouba",
+		      "Trouba.lua"
+		    },
+		    { "Trouba",
+		      "Trouba.lua"
+		    }}
+
+end
